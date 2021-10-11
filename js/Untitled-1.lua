b58d847b0ad2ccec59118664966ea8f6e39173d1
Frank57558 <84279832+Frank57558@users.noreply.github.com>
2021-10-11 12:34:35 -0500

    Add files via upload

---
+++ js/Untitled-1.lua
@@ -0,0 +1,32 @@
+
+let cancion=document.getElementById("cancion");
+let cancion=document.getElementById("cancion1");
+let cancion=document.getElementById("cancion2");
+let cancion=document.getElementById("cancion3");
+let cancion=document.getElementById("cancion4");
+let cancion=document.getElementById("cancion5");
+let cancion=document.getElementById("cancion6");
+let cancion=document.getElementById("cancion7");
+let cancion=document.getElementById("cancion8");
+
+let controls=document.querySelector("controls");
+
+let play_btn=document.getElementById("play_btn");
+let play1_btn=document.getElementById("play1-btn");
+let play2_btn=document.getElementById("play2-btn");
+let play3_btn=document.getElementById("play3-btn");
+let play4_btn=document.getElementById("play4-btn");
+let play5_btn=document.getElementById("play5-btn");
+let play6_btn=document.getElementById("play6-btn");
+let play7_btn=document.getElementById("play7-btn");
+let play8_btn=document.getElementById("play8-btn");
+
+play_btn.addEventListener("click", () =>{
+    
+    cancion.A();
+    cover.style.animationPlayState ="running";
+});
+
+
+
+}
---
+++ js/scrypt.js
@@ -0,0 +1,45 @@
+let cancion = document.getElementById("cancion");
+let play = document.getElementById("play");
+play.addEventListener("click", () =>{
+  cancion.play();
+})
+let cancion1 = document.getElementById("cancion1");
+let play1 = document.getElementById("play1");
+play1.addEventListener("click", () =>{
+  cancion1.play();
+})
+let cancion2 = document.getElementById("cancion2");
+let play2 = document.getElementById("play2");
+play2.addEventListener("click", () =>{
+  cancion2.play();
+})
+let cancion3 = document.getElementById("cancion3");
+let play3 = document.getElementById("play3");
+play3.addEventListener("click", () =>{
+  cancion3.play();
+})
+let cancion4 = document.getElementById("cancion4");
+let play4 = document.getElementById("play4");
+play4.addEventListener("click", () =>{
+  cancion4.play();
+})
+let cancion5 = document.getElementById("cancion5");
+let play5 = document.getElementById("play5");
+play5.addEventListener("click", () =>{
+  cancion5.play();
+})
+let cancion6 = document.getElementById("cancion6");
+let play6 = document.getElementById("play6");
+play6.addEventListener("click", () =>{
+  cancion6.play();
+})
+let cancion7 = document.getElementById("cancion7");
+let play7 = document.getElementById("play7");
+play7.addEventListener("click", () =>{
+  cancion7.play();
+})
+let cancion8 = document.getElementById("cancion8");
+let play8 = document.getElementById("play8");
+play8.addEventListener("click", () =>{
+  cancion8.play();
+})
